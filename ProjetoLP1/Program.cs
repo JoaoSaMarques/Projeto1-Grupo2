292fb90faf41eb8cb031ddc19cbe1b164197c27d
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-04 13:30:13 +0100

    Added cases and toggles on each of the lamps

--- ProjetoLP1/Program.cs
+++ ProjetoLP1/Program.cs
@@ -17,6 +17,38 @@ void ToggleLamps(int index1, int index2)
     ToggleLamp(index2);
 }
 
+//Call this in main to start the program.
+void PlayGame()
+{
+    //As told in the project we will have a maximum of 6 turns to successfully do the puzzle.
+    int numTurns = 0;
+    //While below those 6 turns then:
+    while (numTurns < 6 && (lamps[0] == LampState.Off || lamps[1] == LampState.Off || lamps[2] == LampState.Off))
+    {
+        //It will tell you to press the buttons
+        Console.WriteLine("Press a button (1, 2, or 3):");
+        //Convert value into an int.
+        int button = int.Parse(Console.ReadLine());
+
+        //Switch the int into cases.
+        switch (button)
+        {
+            case 1:
+                ToggleLamp(0);
+                break;
+            case 2:
+                ToggleLamps(0, 1);
+                break;
+            case 3:
+                ToggleLamps(1, 2);
+                break;
+            default:
+                Console.WriteLine("Invalid button.");
+                break;
+        }
+    }
+}
+
 namespace ProjetoLP1
 {
     
@@ -24,7 +56,7 @@ namespace ProjetoLP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PlayGame()
         }
     }
 }
