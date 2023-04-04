be926dc51ba5c8f4c85232ada689b468a9f3b4c1
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-04 12:52:41 +0100

    Added lamp states

--- ProjetoLP1/Program.cs
+++ ProjetoLP1/Program.cs
@@ -1,7 +1,18 @@
 ﻿using System;
 
+//Define the state of which the lamps will be at
+public enum LampState
+{
+    On,
+    Off
+}
+
+//Create a list for the states (all of them start off)
+LampState[] lamps = new LampState[3] { LampState.Off, LampState.Off, LampState.Off };
+
 namespace ProjetoLP1
 {
+    
     class Program
     {
         static void Main(string[] args)
